cec2b38b5f200f16411670fa1cfd8c0098abcc62
Chaz Cornwall <chazcornwall@gmail.com>
2020-09-18 09:24:24 -0600

    Created Derivative namespace and include folder

--- src/task2/derivativeapprox.cpp
+++ src/task2/derivativeapprox.cpp
@@ -1,29 +1,9 @@
-#include<math.h>
+#include <math.h>
+#include "../../include/Derivative.hpp"
 
-class derivativeApprox {
-    private double h;
-    private double function_at_a;
-    private double function_at_a_plus_h;
-    private double function_at_a_minus_h;
+using namespace Derivative;
 
-    derivativeApprox(double h, double minus_h, double a, double plus_h)
-    {
-        function_at_a = a;
-        function_at_a_plus_h = plus_h;
-        function_at_a_minus_h = minus_h
-    }
-
-    void calculate2h()
-    {
-
-    }
-
-    void calculateh()
-    {
-
-    }
-}
 void main()
 {
-    //
+    derivativeApprox();
 }
