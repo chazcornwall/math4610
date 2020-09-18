6aefc922a3d3b21571752f1944323f0c629ed3df
Chaz Cornwall <chazcornwall@gmail.com>
2020-09-18 09:04:37 -0600

    start task2

---
+++ src/task2/derivativeapprox.cpp
@@ -0,0 +1,29 @@
+#include<math.h>
+
+class derivativeApprox {
+    private double h;
+    private double function_at_a;
+    private double function_at_a_plus_h;
+    private double function_at_a_minus_h;
+
+    derivativeApprox(double h, double minus_h, double a, double plus_h)
+    {
+        function_at_a = a;
+        function_at_a_plus_h = plus_h;
+        function_at_a_minus_h = minus_h
+    }
+
+    void calculate2h()
+    {
+
+    }
+
+    void calculateh()
+    {
+
+    }
+}
+void main()
+{
+    //
+}
