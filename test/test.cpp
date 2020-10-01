4f1860784fca473caa9943e5f04b5a39c3af6d8b
Chaz Cornwall <chazcornwall@gmail.com>
2020-10-01 09:59:02 -0600

    Update libaray test

--- test/test.cpp
+++ test/test.cpp
@@ -1,9 +1,12 @@
 #include "../include/math4610lib.hpp"
+#include <iostream>
 
 using namespace MachineEpsilon;
 
 int main(void)
 {
+    std::cout << "Call seps!" << std::endl;
     seps();
+    std::cout << "Call deps!" << std::endl;
     deps();
 }
