a24efc220eb06b2b7783f6e0a3e6fa886ace1e10
Chaz Cornwall <chazcornwall@gmail.com>
2020-10-29 20:07:53 -0600

    start writing in tasksheet 5

--- tasksheets/tasksheet_05.md
+++ tasksheets/tasksheet_05.md
@@ -2,3 +2,25 @@
 [See here](https://github.com/jvkoebbe/math4610/blob/master/tasksheets/tasksheet_05/pdf/tasksheet_05.pdf) for problem set.
 
 <hr>
+
+**Task 1**
+This task has been completed. The image included in Task 3 shows the output of the example function 
+
+**Task 2**
+This task has been completed. The image included in Task 3 shows the output of the example function
+
+**Task 3 and Task 4**
+![](../../test/newtonanalysis_trend.JPG)
+
+The image above shows:
+1. The output and current error of Rootfinding::newtonMethod()
+2. The output and current error of Rootfinding::secantMethod()
+3. The trend lines that match the error reduction rate
+
+The error printed to the console is the log10() of the actual error. The trend data was produced by Regression::linreg(). In this instance, newton's method had an approximate computational convergence rate of 2.49, which is slightly better than quadratic convergence. The secant method's approximate rate of computational convergence was 1.87, which is also slightly better than its mathematical convergence rate of 1.62.
+
+**Task 5**
+This task has been completed.
+
+**Task 6**
+Rootfinding is a pratical task in field such as science and engineering. Due to the increasing difficulty of finding roots as the order of polynomials increase (no analytical solution exists for 5th order or greater), iteration must be used. Common methods for finding roots are bisection, newton's method, and secant method. The main drawback of newton's method is the necessity of calculating a derivative at every iteration. [http://www.math.niu.edu/~dattab/MATH435.2013/ROOT_FINDING.pdf](http://www.math.niu.edu/~dattab/MATH435.2013/ROOT_FINDING.pdf)
--- test/newtonsecantanalysis.cpp
+++ test/newtonsecantanalysis.cpp
@@ -18,13 +18,30 @@ int main()
     std::cout << "Root (Newton Method): " << Rootfinding::newtonsMethod(function, x0, 0.01, 1000, true) << std::endl;
     std::cout << "Root (Secant Method): " << Rootfinding::secantMethod(function, x0, 0.01, 1000, true) << std::endl;
     // zero at 0.8055 
-    double newtonInputs[12] = {-1.087, -1.0633, -1.04461, -1.02456,
-                             -1.00327, -0.981335, -0.960558, -0.945529,
-                             -0.946943, -0.988299, -1.11976, -1.44828};
-    double newtonOutputs[12] = {-1.0633, -1.04461, -1.02456,
-                             -1.00327, -0.981335, -0.960558, -0.945529,
-                             -0.946943, -0.988299, -1.11976, -1.44828,
-                             -2.1894};
+    double newtonInputs[12] = {-1.087,
+                               -1.0633, 
+                               -1.04461, 
+                               -1.02456,
+                               -1.00327, 
+                               -0.981335, 
+                               -0.960558, 
+                               -0.945529,
+                               -0.946943, 
+                               -0.988299, 
+                               -1.11976, 
+                               -1.44828};
+    double newtonOutputs[12] = {-1.0633, 
+                                -1.04461, 
+                                -1.02456,
+                                -1.00327, 
+                                -0.981335, 
+                                -0.960558, 
+                                -0.945529,
+                                -0.946943, 
+                                -0.988299, 
+                                -1.11976, 
+                                -1.44828,
+                                -2.1894};
     double secantInputs[18] = {-1.33709,
                             -1.20175,
                             -1.2339,
@@ -65,6 +82,9 @@ int main()
     double newtonX[2];
     double secantX[2]; 
     Regression::linreg(newtonInputs, newtonX, newtonOutputs, 12);
-    Regression::linreg(secantInputs, secantX, secantOutputs, 18); 
+    Regression::linreg(secantInputs, secantX, secantOutputs, 18);
+
+    std::cout << "Trend of newton method: y = " << newtonX[1] << "x + " << newtonX[0] << std::endl;
+    std::cout << "Trend of secant method: y = " << secantX[1] << "x + " << secantX[0] << std::endl; 
     return 0;
 }
