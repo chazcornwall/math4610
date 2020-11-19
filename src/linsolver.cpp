04cde1364c3f60a4370f49ba89f97a1dd3aa8f86
Chaz Cornwall <chazcornwall@gmail.com>
2020-11-19 08:07:45 -0700

    Create functions for matrices and solving systems of equations

--- src/linsolver.cpp
+++ src/linsolver.cpp
@@ -19,17 +19,86 @@ LinearAlgebra::Matrix::Matrix(const int & numRows, const int & numCols, const do
                     {
                         this->data[row][col] = value;
                     }
+                    else
+                    {
+                        this->data[row][col] = 0.0;
+                    }
                     break;
                 LWR:
                     if(col < row)
                     {
                         this->data[row][col] = value;
                     }
+                    else
+                    {
+                        this->data[row][col] = 0.0;
+                    }
                     break;
                 DIAG:
                     if(col == row)
                     {
                         this->data[row][col] = value;
+                    }
+                    else
+                    {
+                        this->data[row][col] = 0.0;
+                    }
+                    break;
+                default:
+                    this->data[row][col] = value;
+                    break;
+            }
+            
+        }
+    }
+}
+
+LinearAlgebra::Matrix::Matrix(const int & numRows, const int & numCols, LinearAlgebra::MatrixType type) : NUM_ROWS(static_cast<size_t>(numRows)),
+                                                                                                          NUM_COLS(static_cast<size_t>(numCols))
+{
+    this->data = new double*[numRows];
+
+    srand(time(NULL)); // Initialize random number generator
+    double value;
+    
+    // Allocate space for rows and initialize to zero
+    for(size_t row = 0; row < NUM_ROWS; row++)
+    {
+        this->data[row] = new double[numCols];
+        for(size_t col = 0; col < NUM_COLS; col++)
+        {
+            int32_t randomNum = rand() % 1000 + 1;
+            value = static_cast<double>(randomNum);
+            switch(type)
+            {
+                case UPPR:
+                    if(col >= row)
+                    {
+                        this->data[row][col] = value;
+                    }
+                    else
+                    {
+                        this->data[row][col] = 0.0;
+                    }
+                    break;
+               case LWR:
+                    if(col <= row)
+                    {
+                        this->data[row][col] = value;
+                    }
+                    else
+                    {
+                        this->data[row][col] = 0.0;
+                    }
+                    break;
+                case DIAG:
+                    if(col == row)
+                    {
+                        this->data[row][col] = value;
+                    }
+                    else
+                    {
+                        this->data[row][col] = 0.0;
                     }
                     break;
                 default:
@@ -42,7 +111,7 @@ LinearAlgebra::Matrix::Matrix(const int & numRows, const int & numCols, const do
 }
 
 LinearAlgebra::Matrix::Matrix(const int & numRows, const int & numCols, const double & value) : NUM_ROWS(static_cast<size_t>(numRows)),
-                                                                                                                                NUM_COLS(static_cast<size_t>(numCols))
+                                                                                                NUM_COLS(static_cast<size_t>(numCols))
 {
     this->data = new double*[numRows];
     
@@ -57,6 +126,22 @@ LinearAlgebra::Matrix::Matrix(const int & numRows, const int & numCols, const do
     }
 }
 
+LinearAlgebra::Matrix LinearAlgebra::Matrix::duplicate() const
+{
+    LinearAlgebra::Matrix output(static_cast<int>(this->NUM_ROWS), static_cast<int>(this->NUM_COLS), 0.0);
+
+    // Allocate space for rows and initialize to zero
+    for(size_t row = 0; row < NUM_ROWS; row++)
+    {
+        for(size_t col = 0; col < NUM_COLS; col++)
+        {
+            output.data[row][col] = this->data[row][col];
+        }
+    }
+
+    return output;
+}
+
 size_t LinearAlgebra::Matrix::getNumRows() const
 {
     return this->NUM_ROWS;
@@ -67,31 +152,118 @@ size_t LinearAlgebra::Matrix::getNumCols() const
     return this->NUM_COLS;
 }
 
-LinearAlgebra::Matrix LinearAlgebra::Matrix::solve(const LinearAlgebra::Matrix & b) const
+/**********************************************************************************************************************
+*SOLVING LINEAR SYSTEM OF EQUATIONS OPERATIONS
+***********************************************************************************************************************/
+
+LinearAlgebra::Matrix LinearAlgebra::Matrix::reduceRowEchelon(LinearAlgebra::Matrix & b) const
 {
-    size_t numRow = b.getNumRows();
-    size_t numCol = 1;
-    LinearAlgebra::Matrix output(static_cast<int>(numRow), 1, 0.0);
+    LinearAlgebra::Matrix output = this->duplicate();
 
-    return output;
-}
-
-LinearAlgebra::Matrix LinearAlgebra::Matrix::operator+(const LinearAlgebra::Matrix & operand) const
-{
-    size_t numRow = operand.getNumRows();
-    size_t numCol = operand.getNumCols();
-    LinearAlgebra::Matrix output(static_cast<int>(numRow), static_cast<int>(numCol), 0.0, LinearAlgebra::SQR);
-
-    for(size_t row = 0; row < numRow; row++)
+    for(size_t pivotRow = 0; pivotRow < NUM_ROWS; pivotRow++) // Do row reduction NUM_ROWS times
     {
-        for(size_t col = 0; col < numCol; col++)
+        for(size_t currRow = pivotRow + 1; currRow < NUM_ROWS; currRow++)
         {
-            output.data[row][col] = this->data[row][col] + operand.data[row][col];
+            // Select a factor such that row operation R_curr -> R_curr - factor * R_pivot creates a zero in the [currRow][pivotRow] location
+            if(output.data[pivotRow][pivotRow] == 0)
+            {
+                std::cout << "ERROR: Column vectors are not linearly independent! Offending pivot location: (" << pivotRow << ", " << pivotRow << ")" << std::endl;
+                return output;
+            }
+            double factor = output.data[currRow][pivotRow] / output.data[pivotRow][pivotRow];
+            for(size_t currCol = pivotRow + 1; currCol < NUM_COLS; currCol++)
+            {
+                output.data[currRow][currCol] = output.data[currRow][currCol] - factor * output.data[pivotRow][currCol];
+            }
+
+            b.data[currRow][0] = b.data[currRow][0] - factor * b.data[pivotRow][0];
         }
     }
 
     return output;
 }
+
+LinearAlgebra::Matrix LinearAlgebra::Matrix::backSubstitution(const LinearAlgebra::Matrix & b) const
+{
+    Matrix x(static_cast<int>(NUM_ROWS), 1, 0.0); // NUM_ROWS and NUM_COLS should be equal
+    
+    if(this->data[NUM_ROWS - 1][NUM_COLS - 1] == 0)
+    {
+        std::cout << "ERROR: Column vectors are not linearly independent! Offending pivot location: (" << NUM_ROWS - 1 << ", " << NUM_ROWS - 1 << ")" << std::endl;
+        return x;
+    }
+    x.data[x.NUM_ROWS - 1][0] = b.data[b.NUM_ROWS - 1][0] / this->data[NUM_ROWS - 1][NUM_COLS - 1];
+    for(size_t row = NUM_ROWS - 2; row >= 0 && row < NUM_ROWS - 1; row--) // Start at the second to last row
+    {
+        double sum = 0.0;
+        for(size_t col = row + 1; col < NUM_COLS; col++)
+        {
+            sum += this->data[row][col] * x.data[col][0]; // Calculate sum using results from past iterations
+        }
+        if(this->data[row][row] == 0)
+        {
+            std::cout << "ERROR: Column vectors are not linearly independent! Offending pivot location: (" << row << ", " << row << ")" << std::endl;
+            return x;
+        }
+        x.data[row][0] = (b.data[row][0] - sum) / this->data[row][row];
+    }
+
+    return x;
+}
+
+LinearAlgebra::Matrix LinearAlgebra::Matrix::solve(LinearAlgebra::Matrix & b) const
+{
+    // In the equation Ax = b, neither A nor b or changed in this function. 
+    Matrix bDuplicate = b.duplicate();
+    Matrix reduced = reduceRowEchelon(bDuplicate); 
+    Matrix x = reduced.backSubstitution(bDuplicate);
+    return x;
+}
+
+/**********************************************************************************************************************
+*MISCELLANEOUS MATRIX OPERATIONS
+***********************************************************************************************************************/
+
+LinearAlgebra::Matrix LinearAlgebra::Matrix::transpose() const
+{
+    Matrix output(this->NUM_COLS, this->NUM_ROWS, 0.0);
+
+    for(size_t row = 0; row < this->NUM_ROWS; row++)
+    {
+        for(size_t col = 0; col < this->NUM_COLS; col++)
+        {
+            output.data[col][row] = this->data[row][col];
+        }
+    }
+
+    return output;
+}
+
+void LinearAlgebra::Matrix::reduceRowEchelon()
+{
+    for(size_t pivotRow = 0; pivotRow < NUM_ROWS; pivotRow++) // Do row reduction NUM_ROWS times
+    {
+        for(size_t currRow = pivotRow + 1; currRow < NUM_ROWS; currRow++)
+        {
+            // Select a factor such that row operation R_curr -> R_curr - factor * R_pivot creates a zero in the [currRow][pivotRow] location
+            if(this->data[pivotRow][pivotRow] == 0)
+            {
+                std::cout << "ERROR: Column vectors are not linearly independent! Offending pivot location: (" << pivotRow << ", " << pivotRow << ")" << std::endl;
+                return;
+            }
+            double factor = this->data[currRow][pivotRow] / this->data[pivotRow][pivotRow];
+            this->data[currRow][pivotRow] = 0.0;
+            for(size_t currCol = pivotRow + 1; currCol < NUM_COLS; currCol++)
+            {
+                this->data[currRow][currCol] = this->data[currRow][currCol] - factor * this->data[pivotRow][currCol];
+            }
+        }
+    }
+}
+
+/**********************************************************************************************************************
+*PRINTING MATRIX TO CONSOLE
+***********************************************************************************************************************/
 
 void LinearAlgebra::Matrix::print(int minRow, int maxRow, int minCol, int maxCol) const
 {
@@ -114,14 +286,48 @@ void LinearAlgebra::Matrix::print(int minRow, int maxRow, int minCol, int maxCol
     }
 
     std::cout << "*************Matrix**************" << std::endl;
-    for(size_t row = static_cast<size_t>(minRow); row <= static_cast<size_t>(maxRow); row++)
+    for(size_t row = static_cast<size_t>(minRow); row < static_cast<size_t>(maxRow); row++)
     {
         for(size_t col = static_cast<size_t>(minCol); col < static_cast<size_t>(maxCol); col++)
         {
-            std::cout << this->data[row][col] << " ";
+            std::cout << this->data[row][col] << "\t";
         }
         std::cout << std::endl;
     }
+}
+
+void LinearAlgebra::Matrix::print() const
+{
+    std::cout << "*************Matrix**************" << std::endl;
+    for(size_t row = 0; row < NUM_ROWS; row++)
+    {
+        for(size_t col = 0; col < NUM_COLS; col++)
+        {
+            std::cout << this->data[row][col] << "\t";
+        }
+        std::cout << std::endl;
+    }
+}
+
+/**********************************************************************************************************************
+*OPERATOR OVERLOADS
+***********************************************************************************************************************/
+
+LinearAlgebra::Matrix LinearAlgebra::Matrix::operator+(const LinearAlgebra::Matrix & operand) const
+{
+    size_t numRow = operand.getNumRows();
+    size_t numCol = operand.getNumCols();
+    LinearAlgebra::Matrix output(static_cast<int>(numRow), static_cast<int>(numCol), 0.0, LinearAlgebra::SQR);
+
+    for(size_t row = 0; row < numRow; row++)
+    {
+        for(size_t col = 0; col < numCol; col++)
+        {
+            output.data[row][col] = this->data[row][col] + operand.data[row][col];
+        }
+    }
+
+    return output;
 }
 
 LinearAlgebra::Matrix LinearAlgebra::Matrix::operator+=(const LinearAlgebra::Matrix & operand) const
@@ -140,20 +346,139 @@ LinearAlgebra::Matrix LinearAlgebra::Matrix::operator+=(const LinearAlgebra::Mat
     return *this;
 }
 
+LinearAlgebra::Matrix LinearAlgebra::Matrix::operator*(const double & operand) const
+{
+    size_t numRow = this->NUM_ROWS;
+    size_t numCol = this->NUM_COLS;
+    LinearAlgebra::Matrix output(static_cast<int>(numRow), static_cast<int>(numCol), 0.0);
+
+    for(size_t row = 0; row < numRow; row++)
+    {
+        for(size_t col = 0; col < numCol; col++)
+        {
+            output.data[row][col] = this->data[row][col] * operand;
+        }
+    }
+
+    return output;
+}
+
+LinearAlgebra::Matrix LinearAlgebra::Matrix::operator-(const LinearAlgebra::Matrix & operand) const
+{
+    size_t numRow = operand.getNumRows();
+    size_t numCol = operand.getNumCols();
+    LinearAlgebra::Matrix output(static_cast<int>(numRow), static_cast<int>(numCol), 0.0);
+
+    for(size_t row = 0; row < numRow; row++)
+    {
+        for(size_t col = 0; col < numCol; col++)
+        {
+            output.data[row][col] = this->data[row][col] - operand.data[row][col];
+        }
+    }
+
+    return output;
+}
+
+LinearAlgebra::Matrix LinearAlgebra::Matrix::operator-=(const LinearAlgebra::Matrix & operand) const
+{
+    size_t numRow = this->getNumRows();
+    size_t numCol = this->getNumCols();
+
+    for(size_t row = 0; row < numRow; row++)
+    {
+        for(size_t col = 0; col < numCol; col++)
+        {
+            this->data[row][col] = this->data[row][col] - operand.data[row][col];
+        }
+    }
+
+    return *this;
+}
+
+/**********************************************************************************************************************
+*TESTING
+***********************************************************************************************************************/
+
 #ifdef TEST_
 int main()
 {
-    LinearAlgebra::Matrix A(500, 500, 0.0);
-    LinearAlgebra::Matrix B(500, 500, 0.0);
+    size_t numRows = 10;
+    size_t numCols = 10;
+    LinearAlgebra::Matrix A(numRows, numCols, LinearAlgebra::SQR);
+    LinearAlgebra::Matrix B(numRows, numCols, 1.0);
+    LinearAlgebra::Matrix D(numRows, numCols, 1.0);
+    LinearAlgebra::Matrix b(numRows, numCols, 1.0);
 
-    A.data[0][0] = 1.0;
-    B.data[0][0] = 1.0;
+    std::cout << "Testing for creation of different matrix types..." << std::endl;
+    LinearAlgebra::Matrix Random(numRows, numCols, LinearAlgebra::SQR);
+    Random.print();
+    LinearAlgebra::Matrix Random1(numRows, numCols, LinearAlgebra::DIAG);
+    Random1.print();
+    LinearAlgebra::Matrix Random2(numRows, numCols, LinearAlgebra::LWR);
+    Random2.print();
+    LinearAlgebra::Matrix Random3(numRows, numCols, LinearAlgebra::UPPR);
+    Random3.print();
 
+    std::cout << "Testing reduction on already-reduced matrix..." << std::endl;
+    Random3.reduceRowEchelon();
+    Random3.print();
+
+    // Test for "not linearly independent" error message
+    std::cout << "Testing for linear dependendence..." << std::endl;
+    D.reduceRowEchelon();
+
+    Random.reduceRowEchelon();
+    Random.print();
+
+    /*
+    double equation0[3] = {1.0, 5.0, 54.0};
+    double equation1[3] = {2.0, 3.0, 2.0};
+    double equation2[3] = {3.0, 22.0, 63.0};
+
+    A.data[0] = equation0;
+    A.data[1] = equation1;
+    A.data[2] = equation2;
+    */
+
+    std::cout << "Testing transpose operation..." << std::endl;
+    LinearAlgebra::Matrix E = A.transpose();
+    E.print();
+
+    std::cout << "Testing solution for matrix equation..." << std::endl;
+    LinearAlgebra::Matrix x = A.solve(b);
+
+    bool testLinearSolver = true;
+    for(size_t row = 0; row < A.getNumRows(); row++)
+    {
+        double sum = 0.0;
+        for(size_t col = 0; col < A.getNumCols(); col++)
+        {
+            sum += A.data[row][col] * x.data[col][0];
+        }
+
+        testLinearSolver = (abs(sum - b.data[row][0]) < 0.001);
+        if(!testLinearSolver)
+        {
+            std::cout << "Row " << row << " is not solved by x." << std::endl;
+            std::cout << "Sum was " << sum << " , but should be " << b.data[row][0] << std::endl;
+        }
+    }
+
+    if(testLinearSolver)
+    {
+        std::cout << "System solved correctly!" << std::endl;
+    }
+
+    std::cout << "Testing operator overloads..." << std::endl;
     LinearAlgebra::Matrix C = A + B;
-    A += B;
+    LinearAlgebra::Matrix F = A * 5.0;
+    LinearAlgebra::Matrix J = F - A;
+    F.print();
+    J.print();
     std::cout << C.data[0][0] << std::endl;
     std::cout << A.data[0][0] << std::endl;
-    A.print(0, 30, 0, 30);
+    C.print(0, 30, 0, 30);
 
     return 0;
 }
